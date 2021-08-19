2731313e7f2be48004f0c340000e740e5a03946e
Daisy Lin <daisy.lin@access-company.com>
2021-08-19 22:01:06 +0800

    'init'

---
+++ Daisy portfolio/js/main.js
@@ -0,0 +1,20 @@
+$ = jQuery;
+
+$(window).scroll(function () {
+  // 設置整個網頁的高度
+  let scrollEnd = document.documentElement.scrollHeight;
+
+  var scroolNow = $(window).scrollTop();
+  var opacityVal = scroolNow / scrollEnd;
+
+  document.getElementsByClassName("bg1")[0].style.opacity = 1 - opacityVal;
+  document.getElementsByClassName("bg2")[0].style.opacity = opacityVal;
+});
+
+$(document).ready(function () {
+    document.getElementById('container').style.display = 'none';
+    setTimeout(function(){
+        document.getElementById('load').style.display = 'none';
+        document.getElementById('container').style.display = 'block';
+    }, 7000);
+});
