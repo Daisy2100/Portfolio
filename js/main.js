138cf5b2fe963f3182dbe12a80db758e7fd3f301
Daisy Lin <daisy.lin@access-company.com>
2021-08-22 21:09:03 +0800

    RWD

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 $ = jQuery;
 
 $(window).scroll(function () {
+  // 底圖變色控制
   // 設置整個網頁的高度
   let scrollEnd = document.documentElement.scrollHeight;
-
   var scroolNow = $(window).scrollTop();
-  var opacityVal = scroolNow / scrollEnd / 1.2 ;
+  var opacityVal = scroolNow / scrollEnd / 1.4 ;
 
   document.getElementsByClassName("bg1")[1].style.opacity = (1 - opacityVal);
   document.getElementsByClassName("bg2")[0].style.opacity = opacityVal;
+
+  //text-section 透明度控制
+  for (var i =0; i<document.getElementsByClassName('text-section').length; i++){
+    var textOpacity = getPosition(document.getElementsByClassName('text-section')[i])['y'];
+    document.getElementsByClassName('text-section')[i].style.opacity = scroolNow/textOpacity;
+  }
 });
 
 
@@ -25,7 +31,12 @@ document.getElementById('closebtn').addEventListener('click',function(){
   document.getElementById('openbtn').style.display="block"
   document.getElementById('over').style.display = "none";
 })
+document.getElementById('over').addEventListener('click',function(){
+  document.getElementById('openbtn').style.display="block"
+  document.getElementById('over').style.display = "none";
+})
 
+// loading 動畫
 $(document).ready(function () {
   document.getElementById("container").style.display = "none";
   setTimeout(function () {
@@ -38,3 +49,17 @@ $(document).ready(function () {
 // document.getElementById("container").style.display = "block";
 
 
+
+
+function getPosition (element) {
+  var x = 0;
+  var y = 0;
+  // 搭配上面的示意圖可比較輕鬆理解為何要這麼計算
+  while ( element ) {
+    x += element.offsetLeft - element.scrollLeft + element.clientLeft;
+    y += element.offsetTop - element.scrollLeft + element.clientTop;
+    element = element.offsetParent;
+  }
+
+  return { x: x, y: y };
+}
