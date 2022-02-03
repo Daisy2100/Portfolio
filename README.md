8b37962d88bd83ff220d7c4af696766080befc7f
Daisy Lin <daisy.lin@access-company.com>
2022-02-03 20:56:14 +0800

    add wrok images

--- README.md
+++ README.md
@@ -16,6 +16,10 @@ https://fontawesome.com/v5.15/icons?d=gallery&p=2
 <i class="fa fa-thumbs-o-up fa-5x"></i>
 ```
 
+# 配色參考
+
+https://coolors.co/012a4a-013a63-01497c-014f86-2a6f97-2c7da0-468faf-61a5c2-89c2d9-a9d6e5
+
 ---
 
 # Sass 建置筆記與指令
--- js/main.js
+++ js/main.js
@@ -16,29 +16,6 @@ $(window).scroll(function () {
   //   document.getElementsByClassName('text-section')[i].style.opacity = scroolNow/textOpacity;
   // }
 
-  //淡入動畫
-  var top = document.documentElement.scrollTop || document.body.scrollTop
-  // var SE = document.getElementById('SE');
-  var skill = document.getElementById('skill');
-  var work = document.getElementById('work');
-  // var contact = document.getElementById('contact');
-  // if (top > 200){
-  //   SE.style.animation = "fadeIn 3s";
-  //   SE.style.marginTop = "0";
-  // }
-  if (top > (skill.offsetTop-skill.offsetHeight)){
-    skill.style.animation = "fadeIn 3s";
-    skill.style.marginTop = "0";
-  }
-  if (top > (work.offsetTop-work.offsetHeight)){
-    work.style.animation = "fadeIn 3s";
-    work.style.marginTop = "0";
-  }
-  // if (top > (contact.offsetTop-contact.offsetHeight)){
-  //   contact.style.animation = "fadeIn 3s";
-  //   contact.style.marginTop = "0";
-  // }
-
 });
 
 
@@ -59,19 +36,6 @@ document.getElementById('over').addEventListener('click',function(){
   document.getElementById('openbtn').style.display="block"
   document.getElementById('over').style.display = "none";
 })
-
-// loading 動畫
-$(document).ready(function () {
-  document.getElementById("container").style.display = "none";
-  setTimeout(function () {
-    document.getElementById("load").style.display = "none";
-    document.getElementById("container").style.display = "block";
-  }, 3000);
-});
-
-// document.getElementById("load").style.display = "none";
-// document.getElementById("container").style.display = "block";
-
 
 
 /**
