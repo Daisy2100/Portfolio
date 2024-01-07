11ede057e1f9c2bcab8fd1c4d5b680a44b4709d8
10646028 <10646028@ntub.edu.tw>
2024-01-07 22:04:59 +0800

    調整路徑

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
-import './assets/main.css'
+import './assets/style/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import FontAwesomeIcon from "./utilities/fontawesome-icons";
+
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app.component("FontAwesomeIcon", FontAwesomeIcon);
+app.use(router);
+app.mount("#app");
